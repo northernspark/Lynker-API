fac61aec24023046557dd8f2bb76a40c9edc390b
NorthernSpark <joe@northernspark.co.uk>
2015-09-11 01:47:55 +0100

    User Updates
    network now adds support for tokens. suggested returns suggested users
    from networks username

--- user/usernetworks.php
+++ user/usernetworks.php
@@ -11,6 +11,7 @@ $passed_username = $passed_data['user'];
 $passed_page = strip_tags($passed_data['page']);
 $passed_status = $passed_data['status'];
 $passed_data = $passed_data['data'];
+$passed_token = $passed_data['token'];
 
 if (empty($passed_status)) $passed_status = 'active';
 
@@ -27,11 +28,11 @@ else {
 	$network_data = mysql_fetch_assoc($network_query);
 	if ($network_count == 0) {
 		$network_key = "nw_" . generate_key();
-		$network_post = mysql_query("INSERT INTO `networks` (`network_id`, `network_key`, `network_type`, `network_user`, `network_status`, `network_token`, `network_username`, `network_page`, `network_data`) VALUES (NULL, '$network_key', '$passed_type', '$user_key', '$passed_status', '', '$passed_username', 'passed_page', '$passed_data');");
+		$network_post = mysql_query("INSERT INTO `networks` (`network_id`, `network_key`, `network_type`, `network_user`, `network_status`, `network_token`, `network_username`, `network_page`, `network_data`) VALUES (NULL, '$network_key', '$passed_type', '$user_key', '$passed_status', '$passed_token', '$passed_username', '$passed_page', '$passed_data');");
 	}
 	else {
 		$network_key = $network_data['network_key'];			
-		$network_post = mysql_query("UPDATE `networks` SET `network_status` = '$passed_status', `network_token` = '$passed_token', `network_username` = '$passed_username', `network_page` = '$passed_page', `network_data` = '$passed_data' WHERE `network_key` LIKE '$network_key';");
+		$network_post = mysql_query("UPDATE `networks` SET `network_status` = '$passed_status', `network_token` = '$passed_token', `network_token` = '$passed_token', `network_username` = '$passed_username', `network_page` = '$passed_page', `network_data` = '$passed_data' WHERE `network_key` LIKE '$network_key';");
 				
 	}
 	
---
+++ user/usersuggested.php
@@ -0,0 +1,52 @@
+<?
+
+include '../lib/auth.php';
+include '../lib/slack.php';
+
+header('Content-Type: application/json');
+
+$passed_data = json_decode(file_get_contents('php://input'), true);
+$passed_networks = explode(",", $passed_data['networks']);
+
+if (empty($passed_networks) || count($passed_networks) == 0) {
+	$json_status = 'atleast one network paramater required';
+	$json_output[] = array('status' => $json_status, 'sucsess' => 'false');
+	echo json_encode($json_output);
+	exit;
+	
+}
+else {
+	foreach ($passed_networks as $item) {
+		$network_query = mysql_query("SELECT * FROM `networks` WHERE `network_username` LIKE '$item' LIMIT 0 ,1");
+		$network_data = mysql_fetch_assoc($network_query);
+		$network_user = $network_data['network_user'];
+		$network_type = $network_data['network_type'];
+				
+		$user_query = mysql_query("SELECT * FROM `users` LEFT JOIN `companies` on `users`.`user_company` LIKE `companies`.`company_key`  WHERE `user_status` LIKE 'active' AND `user_key` LIKE '$network_user' AND `user_key` NOT LIKE '$user_key' ORDER BY `user_name` ASC LIMIT 0, 1");
+		$user_count = mysql_num_rows($user_query);
+		while($row = mysql_fetch_array($user_query)) {	
+			$user_name = $row['user_name'];
+			$user_key = $row['user_key'];
+			$user_company = str_replace("&#39;", "'" ,$row['company_name']);	
+			$user_profile = explode(",", $row['user_profile']);
+			$user_nickname = $row['user_nickname'];
+			$user_headline = $row['user_headline'];
+			$user_location = $row['user_location'];
+			$user_emails = explode(",", $row['user_emails']);
+			$user_status = $row['user_status'];
+			$user_type = $row['user_type'];
+			
+			$user_data[] = array('name' => $user_name, 'profile' => $user_profile[0], 'key' => $user_key, 'location' => $user_location, 'headline' => $user_headline, 'company' => $user_company, 'status' => $user_status, 'network' => $network_type);
+			
+		}
+			
+	}
+	
+	$json_status = 'returned ' . $user_count . ' users';
+	$json_output[] = array('status' => $json_status, 'sucsess' => 'true', 'results' => $user_data);
+	echo json_encode($json_output);
+	exit;
+	
+}
+			
+?>
